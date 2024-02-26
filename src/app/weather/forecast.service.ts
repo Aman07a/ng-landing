00f3e7524c8626ce8ceb6fcbacc074b20867f7b4
Aman07a <aman16.aa17@gmail.com>
2024-02-26 11:54:40 +0100

    Geolocation in an Observable

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,9 +8,14 @@ export class ForecastService {
   constructor() {}
 
   getCurrentLocation() {
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      this.position = position;
-      getForecastData();
+    return new Observable<GeolocationCoordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete();
+        },
+        (err) => observer.error(err)
+      );
     });
   }
 }
