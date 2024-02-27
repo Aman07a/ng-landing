7be90f2f699ad291c477e7ff5ce100251ecdc888
Aman07a <aman16.aa17@gmail.com>
2024-02-27 10:42:58 +0100

    Transforming Coordinates to Query Params

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -7,12 +10,25 @@ import { Observable } from 'rxjs';
 export class ForecastService {
   constructor() {}
 
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map((coords) => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'metric')
+          .set('appid', environment.api_key);
+      })
+    );
+  }
+
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {
       window.navigator.geolocation.getCurrentPosition(
         (position) => {
           observer.next(position.coords);
           observer.complete();
+          console.log(environment.api_key);
         },
         (err) => observer.error(err)
       );
