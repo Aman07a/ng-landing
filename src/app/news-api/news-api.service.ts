2414b19ec35a99070e254473a7e7231c074569ef
Aman07a <aman16.aa17@gmail.com>
2024-02-29 15:22:48 +0100

    Subject Declarations

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { tap, map, switchMap } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 
@@ -7,9 +9,16 @@ import { environment } from 'src/environments/environment';
 })
 export class NewsApiService {
   private url = 'https://newsapi.org/v2/top-headlines';
-  private pageSize = 20;
+  private pageSize = 10;
   private apiKey = environment.api_key;
   private country = 'nl';
 
-  constructor() {}
+  pagesInput: Subject<number>;
+  pagesOutput: Observable<any>;
+  numberOfPages: Subject<number>;
+
+  constructor() {
+    this.pagesInput = new Subject();
+    this.pagesOutput = this.pagesInput.pipe();
+  }
 }
