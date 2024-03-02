b4a3338f1f43dfa94b5908136a75886bfee86ace
Aman07a <aman16.aa17@gmail.com>
2024-03-02 09:49:42 +0100

    Paginator Internals

--- src/app/shared/paginator/paginator.component.ts
+++ src/app/shared/paginator/paginator.component.ts
@@ -8,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class PaginatorComponent implements OnInit {
   // TODO: make sure we receive this value from the parent component
   numberOfPages = 5;
+  currentPage = 1;
 
-  constructor() {}
+  pageOptions: number[] | any;
+
+  constructor() {
+    this.pageOptions = [
+      this.currentPage - 2,
+      this.currentPage - 1,
+      this.currentPage,
+      this.currentPage + 2,
+      this.currentPage + 1,
+    ].filter(
+      (pageNumber) => pageNumber >= 1 && pageNumber <= this.numberOfPages
+    );
+  }
 
   ngOnInit() {}
 }
